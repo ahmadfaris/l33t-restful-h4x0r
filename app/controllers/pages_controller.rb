7db38660de8cd377423eff0e1545ebca2cea21a8
Wan Ahmad Faris <faris91@gmail.com>
2017-02-20 00:50:55 +0800

    Test local POST api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def test
+  	render(
+      json: {
+        message: 'Test status code',
+        status: 400
+      }
+    )
+  end
 end
